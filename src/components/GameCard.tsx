4c3c212dbaf20a72acaef68e77980391fe439fd8
shiyingdong <shiyingdong@webull.com>
2025-02-13 17:38:49 +0800

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,7 @@
-import { Game } from '@/hooks/useGames';
 import { Card, Heading, HStack, Image } from '@chakra-ui/react';
+
+import { Game } from '@/hooks/useGames';
+import getCroppedImageUrl from '@/services/image-url';
 import CriticScore from './CriticScore';
 import PlatformIconList from './PlatformIconList';
 
@@ -10,7 +12,7 @@ interface GameCardProps {
 const GameCard = ({ game }: GameCardProps) => {
   return (
     <Card.Root borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image} alt={game.name} />
+      <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
       <Card.Body>
         <Heading fontSize='2xl'>{game.name}</Heading>
         <HStack justifyContent='space-between'>
