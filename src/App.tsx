3140033fe91b7750267fa32dea8994f80812156b
shiyingdong <shiyingdong@webull.com>
2025-02-13 15:13:23 +0800

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 
 function App() {
-  return <Button colorPalette='blue'>Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area='nav' bg='coral'>
+        Navbar
+      </GridItem>
+      <GridItem area='aside' hideBelow='lg' bg='gold'>
+        Sidebar
+      </GridItem>
+      <GridItem area='main' bg='dodgerblue'>
+        Main content
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
